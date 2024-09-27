411641379d6ebd2f5c77a0c72b56473bfe27c37d
Donald Haase <quzar86@yahoo.com>
2024-09-26 23:46:01 -0400

    Clean up usage of `INLINE` in utils (#771)
    * Just use c99 inline rather than the custom one. When this was originally written it wasn't as widely supported. Clean up unnessary commented out bits.
    
    * Remove copy-pasted CFLAGS define.
    
    * Clean up copy-pasted CFLAG define and commented out debug option.
    
    ---------
    
    Co-authored-by: QuzarDC <quzar@completext.com>
    Co-authored-by: Falco Girgis <gyrovorbis@gmail.com>

--- utils/vqenc/vq_internal.h
+++ utils/vqenc/vq_internal.h
@@ -4,43 +4,39 @@
 
 #include "vq_types.h"
 
-#ifndef INLINE
-#define INLINE
-#endif
-
-static void INLINE get_color(fcolor_t *c, uint8 *pixels) {
+static void inline get_color(fcolor_t *c, uint8 *pixels) {
     c->a = pixels[0];
     c->r = pixels[1];
     c->g = pixels[2];
     c->b = pixels[3];
 }
 
-static void INLINE sum_colors(fcolor_t *out, fcolor_t *in) {
+static void inline sum_colors(fcolor_t *out, fcolor_t *in) {
     out->a += in->a;
     out->r += in->r;
     out->g += in->g;
     out->b += in->b;
 }
 
-static void INLINE div_colors(fcolor_t *out, float v) {
+static void inline div_colors(fcolor_t *out, float v) {
     out->a /= v;
     out->r /= v;
     out->g /= v;
     out->b /= v;
 }
 
-static void INLINE clear_quad(fquad_t *q) {
+static void inline clear_quad(fquad_t *q) {
     memset(q, '\0', sizeof(*q));
 }
 
-static void INLINE add_quad(fquad_t *out, fquad_t *in) {
+static void inline add_quad(fquad_t *out, fquad_t *in) {
     sum_colors(&out->p[0], &in->p[0]);
     sum_colors(&out->p[1], &in->p[1]);
     sum_colors(&out->p[2], &in->p[2]);
     sum_colors(&out->p[3], &in->p[3]);
 }
 
-static void INLINE sub_quad(fquad_t *out, fquad_t *a, fquad_t *b) {
+static void inline sub_quad(fquad_t *out, fquad_t *a, fquad_t *b) {
     int i;
 
     for(i = 0; i < 4; i++) {
@@ -51,7 +47,7 @@ static void INLINE sub_quad(fquad_t *out, fquad_t *a, fquad_t *b) {
     }
 }
 
-static void INLINE div_quad(fquad_t *q, float v) {
+static void inline div_quad(fquad_t *q, float v) {
     if(v < 1.0f) {
         clear_quad(q);
     }
@@ -63,8 +59,8 @@ static void INLINE div_quad(fquad_t *q, float v) {
     }
 }
 
-static void INLINE copy_quad(fquad_t *out, fquad_t *in) {
+static void inline copy_quad(fquad_t *out, fquad_t *in) {
     *out = *in;
 }
 
-#endif
+#endif  /* __VQ_INTERNAL_H */
